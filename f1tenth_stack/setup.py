eb92acda295df5f52ac4942ea1efe3e7267aa476
Bala Kolanu <bkolanu@purdue.edu>
2026-03-04 11:50:44 -0500

    streamline launches and keep imu-based core workflows

--- f1tenth_stack/setup.py
+++ f1tenth_stack/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'throttle_interpolator = f1tenth_stack.throttle_interpolator:main',
+            'speed_clipper = f1tenth_stack.speed_clipper:main',
             'tf_publisher = f1tenth_stack.tf_publisher:main',
             'scanmatching_tf_publisher = f1tenth_stack.scanmatching_tf_publisher:main',
             'tf_pose_reference_publisher = f1tenth_stack.tf_pose_reference_publisher:main',
